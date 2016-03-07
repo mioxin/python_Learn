7b1dc5f8feff0c79ccf79db2953be531314c0b76
mioxin <mioxin@ya.ru>
2016-03-08 00:04:51 +0600

    new echoserv/serv.py

--- echoserv/serv.py
+++ echoserv/serv.py
@@ -1,33 +1,30 @@
 import asyncore
 import socket
 
-
 class EchoHandler(asyncore.dispatcher_with_send):
 
-    def handler_read(self):
-        data=self.recv(1024)
+    def handle_read(self):
+        data = self.recv(8192)
         if data:
             if data == 'close':
                 self.close()
-            else:
-                print(data)
-                self.send(data)
-
+            print (data)
+            self.send(data)
 
 class EchoServer(asyncore.dispatcher):
 
-    def __init__(self,host,port):
+    def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
-        self.bind((host,port))
+        self.bind((host, port))
         self.listen(10)
 
-    def handler_accept(self):
+    def handle_accept(self):
         pair = self.accept()
         if pair is not None:
             sock, addr = pair
-            print 'Incomming %s' % repr(addr)
+            print 'Incoming connection from %s' % repr(addr)
             handler = EchoHandler(sock)
 
 
