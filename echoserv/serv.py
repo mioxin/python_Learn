ee269d174f8074f604e7223c42d8bb50e41bdc3d
mioxin <mioxin@ya.ru>
2016-03-07 22:47:31 +0600

    3 echoserv/serv.py

--- echoserv/serv.py
+++ echoserv/serv.py
@@ -1,14 +1,16 @@
 import asyncore                                                                 
 import socket                                                                   
                                                               
-class EchoHandler(asyncore.dispatcher_with_send):                               
+class EchoHandler(asyncore.dispatcher_with_send):
+
   def handler_read(self):                                                       
     data=self.recv(1024)                                                        
-    if  data:                                                                   
-      if data=='close' or data=='Close':                                        
+    if data:
+      if data == 'close' :
         self.close()
-      print(data)
-      self.send(data)                                                           
+      else:
+        print(data)
+        self.send(data)                                                           
                                                                                 
 class EchoServer(asyncore.dispatcher):                                          
                                                                                 
@@ -20,10 +22,11 @@ class EchoServer(asyncore.dispatcher):
     self.listen(10)                                                              
                                                                                 
   def handler_accept(self):                                                       
-        pair=self.accept()                                                            
+        pair = self.accept()                                                            
         if pair is not None:                                                          
-            sock,addr=pair                                                              
-            handler=EchoHandler(sock)
+            sock, addr = pair
+            print('Incomming %s' % repr(addr))
+            handler = EchoHandler(sock)
 
-server=EchoServer('0.0.0.0',2222)
+server = EchoServer('0.0.0.0', 2222)
 asyncore.loop() 
